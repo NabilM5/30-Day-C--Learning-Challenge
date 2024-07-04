47a31fa1d263e550363c72a47a317a5c3e1a1e45
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-07-04 12:41:20 +0300

    update

---
+++ second.cpp
@@ -0,0 +1,14 @@
+//
+//  second.cpp
+//  firstprogarm
+//
+//  Created by - on 04.07.2024.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Code with me";
+    return 0;
+}
---
+++ second.hpp
@@ -0,0 +1,13 @@
+//
+//  second.hpp
+//  firstprogarm
+//
+//  Created by - on 04.07.2024.
+//
+
+#ifndef second_hpp
+#define second_hpp
+
+#include <stdio.h>
+
+#endif /* second_hpp */
