8d89f7e269d38c266fded9f20473f55cc19f3122
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-07-07 13:43:13 +0300

    update

--- datatype.cpp
+++ datatype.cpp
@@ -5,7 +5,7 @@ int main() {
 	int age = 25;
 	double height = 1.75;
 	string name = "Nabil";
-	float weight = 65;
+	float weight = 65.5f;
 	cout << "My mane is " << name << ",my age is " << age << ", my height is " << height << " meters, and my weight is " << weight <<  " kg!" << endl;
 	return 0;
 }
