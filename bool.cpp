d103fbfd0723bf4f86cb26255827fcef031e3f13
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-07-08 00:44:25 +0300

    update

--- bool.cpp
+++ bool.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
 	bool faster = true;
 	bool slowly = false;
-	cout << "Are you faster " << faster << "\n";
-	cout << "Are you slowly	" << slowly << endl;
+	cout << std::boolalpha; // This will print bool as true or false.
+	cout << "Are you faster " << faster << "\n"; // Corrected \n and added quotes.
+	cout << "Are you slowly	" << slowly << endl; // Added std::endl to new line.
 	return 0;
 }
