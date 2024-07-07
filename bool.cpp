2858d487b30c404d0003a3fc0112d851e4a9ee01
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-07-08 00:40:00 +0300

    update

---
+++ bool.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool faster = true;
+	bool slowly = false;
+	cout << "Are you faster " << faster << "\n";
+	cout << "Are you slowly	" << slowly << endl;
+	return 0;
+}
